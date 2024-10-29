69256ea2326934306aaa53c29bc3fc8d5f4c1995
Nicklas Ebdrup Hansen <mail2neh@gmail.com>
2024-10-29 13:22:22 +0100

    Update Player.java
    Started creating the beginning of cooperation between the java files

--- CDIO project/Player.java
+++ CDIO project/Player.java
@@ -1,14 +1,35 @@
 import java.util.Scanner;
 //Class to store playername and construction of new players
 class Player {
-  String name;
-  int gold;
+  private String name;
+  private int gold;
+  private int rollTotal;
   
   //Constructor for objects of Player class
-  public player(String name, int gold){
+  public player(String name){
       this.name = name;
       this.gold = 1000;
+      this.rollTotal = 0;
   }
+  
+  public String getName() {
+    return name;
+  }
+
+  public int getGold() {
+    return gold;
+  }
+
+public int[] rollDice (Dice dice) {
+  int [] rolls = dice.rollTwoDice();
+  System.out.println(name + " rolled: " + rolls[0] + " and " + rolls[1]);
+
+  rollTotal = rolls[0] + rolls[1];
+
+  System.out.println(getDescription(rollTotal));
+  
+
+  System.out.println(name + "'s total gold: " + gold);
 }
 //Class to store players gold values
 class Account {
